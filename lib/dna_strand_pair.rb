2d60cb819b15f63c488efe7cd2ab9e31fc3d7a47
mijikim <miji.kim926@gmail.com>
2014-06-26 10:09:31 -0600

    morning exercise completed

--- lib/dna_strand_pair.rb
+++ lib/dna_strand_pair.rb
@@ -1,3 +1,22 @@
 class DnaStrandPair
 
+  def initialize(strand_1, strand_2)
+    @strand_1 = strand_1
+    @strand_2 = strand_2
+  end
+
+  def hamming_distance
+    index = 0
+    count = 0
+    while index < @strand_2.length || index < @strand_1.length
+      if @strand_1[index] == nil || @strand_2[index] == nil
+        count = count + 0
+      elsif @strand_1[index] != @strand_2[index]
+        count = count + 1
+      end
+      index = index + 1
+    end
+    count
+  end
+
 end
